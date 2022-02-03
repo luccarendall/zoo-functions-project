04eadc03f9082d93d67f94b0f96b426f3aac5995
Lucca Rendall <luccarendall1@gmail.com>
2022-02-03 07:08:57 -0300

    Requisito 4

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 function getAnimalsOlderThan(animal, age) {
   const animal = data.species.find((element) => element.name === animal); // Recebe o nome de uma espécie e uma idade
   return animal.residents.every((eachAnimal) => eachAnimal.age >= age); // Retorna os animais que possuem a idade mínima especificada
-
+  
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((gerente) => gerente.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error ('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees.filter((worker) => worker.managers.includes(managerId))
+    .map((relatedEmployees) => `${relatedEmployees.firstName} ${relatedEmployees.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
